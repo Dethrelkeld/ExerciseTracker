f1dd637695dc9dbdb575a914a61e2df815638330
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-03-05 16:27:49 -0600

    fixed routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -30,7 +30,15 @@ router.put("/api/workouts/:id", (req, res) => {
 
 // get request to get last workout
 router.get("/api/workouts/", (req, res) => {
-    Workout.find({})
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
         .then((data) => {
             res.json(data);
         })
@@ -42,14 +50,21 @@ router.get("/api/workouts/", (req, res) => {
 
 // get request to get all workouts
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-        console.log(err);
-      });
+   Workout.aggregate([
+       {
+           $addFields: {
+               totalDuration: {
+                   $sum: "$exercises.duration"
+               }
+           }
+       }
+   ]).sort({_id:-1}).limit(7).then((data) => {
+    res.json(data);
+})
+.catch(err => {
+    res.status(400).json(err);
+    console.log(err);
+});
   });
 
 
