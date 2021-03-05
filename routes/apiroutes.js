f8e70ff1ebf6c793c9967325ec95dec703f01785
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-03-04 21:48:34 -0600

    progress on api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,14 +2,17 @@ const router = require("express").Router();
 const { findById } = require("../models/workout.js");
 const Workout = require("../models/workout.js");
 
+
+//  post to create workout
 router.post("/api/workouts", (req, res) =>{
     Workout.create({}).then(data => {
         res.json(data)
     }).catch(err => {
-        res.json(err)
+        res.status(400).json(err)
     });
 });
 
+//  put request to add to workout
 router.put("/api/workouts/:id", (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, {
         $push:{
@@ -21,19 +24,32 @@ router.put("/api/workouts/:id", (req, res) => {
     ).then(data => {
         res.json(data)
     }).catch(err => {
-        res.json(err)
+        res.status(400).json(err)
     });
-})
+});
 
-// router.get
-// router.get
+// get request to get last workout
+router.get("/api/workouts/", (req, res) => {
+    Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// get request to get all workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.
+});
 
 // find aggregate method from mongoose docs
 
-// router.delete
+// delete to delete a workout
 router.delete("/api/workouts/:id", (req, res) => {
     Workout.findById(req.params.id)
-})
+});
 
 
 module.exports = router;
